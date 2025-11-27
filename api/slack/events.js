a758b0480c91a585486508ba968f8c9105e7ca06
Jayant Aggarwal <jayantaggarwal@MacBook-Air.local>
2025-11-27 19:15:12 +0530

    newr

--- api/slack/events.js
+++ api/slack/events.js
@@ -5,11 +5,9 @@ export default function handler(req, res) {
 
   console.log("Incoming Slack Event:", req.body);
 
-  // Slack URL verification
   if (req.body?.type === "url_verification") {
     return res.status(200).send(req.body.challenge);
   }
 
-  // Mandatory fast response
   return res.status(200).send("OK");
 }
